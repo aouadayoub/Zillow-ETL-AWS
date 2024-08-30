164c7a5059a9a90c6f3050e7367a1cc193f5bbfc
Ayoub <aouadayoub2003@gmail.com>
2024-08-30 19:10:40 +0100

    first commit

---
+++ basic.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS zillowdata (
+    bathrooms NUMERIC,
+    bedrooms NUMERIC,
+    city VARCHAR(255),
+    homeStatus VARCHAR(50),
+    homeType VARCHAR(50),
+    livingArea NUMERIC,
+    price NUMERIC,
+    rentZestimate NUMERIC,
+    zipcode INT
+);
